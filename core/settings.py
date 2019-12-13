d9d3edd2586fa36f9bd7fbacc57ccfd192d4781b
Sam Ireland <sam.ireland.uk@gmail.com>
2019-12-13 12:54:48 +0000

    Start django app

---
+++ core/settings.py
@@ -0,0 +1,24 @@
+import os
+from .secrets import SECRET_KEY, BASE_DIR, DATABASES
+
+ALLOWED_HOSTS = []
+
+DEBUG = True
+
+ROOT_URLCONF = "core.urls"
+
+INSTALLED_APPS = [
+ "django.contrib.contenttypes",
+ "django.contrib.staticfiles",
+ "core"
+]
+
+DATE_FORMAT = "D j M, Y"
+USE_TZ = True
+TIME_ZONE = "UTC"
+
+MIDDLEWARE = [
+ "django.middleware.common.CommonMiddleware",
+]
+
+STATIC_URL = "/static/"
